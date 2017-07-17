c3114779d6194ef0bacf733546e9d1ba5dd2f463
Shane Hathaway <shane@wingcash.com>
2017-07-17 10:14:28 -0600

    Shortened the ack section. Tidied JS.

--- public_html/fpn.js
+++ public_html/fpn.js
@@ -33,7 +33,7 @@ jQuery(document).ready(function ($) {
     if (!/#.+$/.test(this.href)) {
       return;
     }
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
